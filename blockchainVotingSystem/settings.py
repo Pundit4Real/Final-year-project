4ed1fc3bf30844043cc6045d898e488757dbe350
Pundit4Real <mohammedaalli088@gmail.com>
2025-07-11 08:34:57 +0000

    ref: requirement updatd

--- blockchainVotingSystem/settings.py
+++ blockchainVotingSystem/settings.py
@@ -94,10 +94,13 @@ WSGI_APPLICATION = 'blockchainVotingSystem.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
